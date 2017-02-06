57960e80ebfb7c3c3ca8dced1496d6f26c113f15
Danny Guinther <danny@zensight.co>
2017-02-06 12:03:03 -0500

    First stab at reload of zs extensions when assume-user endpoint is hit

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Extensions Reloader for Chrome™
+# zs-extension-reloader
 
-Reloads all unpacked extensions with a single click.
-
-The extension is available for download [here].
-
-If you've ever developed a Google Chrome™ extension, you might have wanted to automate the process of reloading your unpacked extension without the need of going through the extensions page.
-
-"Extensions Reloader" allows you to reload all unpacked extensions using 2 ways:
-
-1. The extension's toolbar button.
-2. Browsing to "http://reload.extensions".
-
-The "reload by browsing" is intended for automating the reload process using "post build" scripts - just browse to "http://reload.extensions", and chrome will open with freshly reloaded extensions.
-
-[here]:https://chrome.google.com/webstore/detail/fimgfedafeadlieiabdeeaodndnlbhid
+Reloads Zensight extensions whenever `assume-user` endpoint is visited.
--- background.js
+++ background.js
@@ -1,83 +1,39 @@
-function reloadExtensions() {
-    
-	// find all unpacked extensions and reload them
-	chrome.management.getAll(function(a) {
-		var ext = {};
-		for (var i = 0; i < a.length; i++) {
-			ext = a[i];
-			if ((ext.installType === "development") &&
-				(ext.enabled === true) &&
-				(ext.name !== "Extensions Reloader")) {
-					console.log(ext.name + " reloaded");
-					(function (extensionId, extensionType) {
-						// disable
-						chrome.management.setEnabled(extensionId, false, function() {
-							// re-enable
-							chrome.management.setEnabled(extensionId, true, function() {
-								// re-launch packaged app
-								if (extensionType === "packaged_app") {
-									chrome.management.launchApp(extensionId);
-								}
-							});
-						});
-					})(ext.id, ext.type);
-			}
-		}
-	});
-
-	// Reload the current tab based on option value
-	chrome.storage.sync.get("reloadPage", function(item) {
-		if (item.reloadPage) {
-            chrome.tabs.getSelected(null, function(tab) {
-			    chrome.tabs.reload(tab.id);
-            });
-		}
-	});
-
-	// show an "OK" badge
-	chrome.browserAction.setBadgeText({text: "OK"});
-	chrome.browserAction.setBadgeBackgroundColor({color: "#4cb749"});
-	setTimeout(function() {
-		chrome.browserAction.setBadgeText({text: ""});
-	}, 1000);
-
+function reloadExtension(extensionId, extensionType) {
+  // disable
+  chrome.management.setEnabled(extensionId, false, function() {
+    // re-enable
+    chrome.management.setEnabled(extensionId, true, function() {
+      // re-launch packaged app
+      if (extensionType === "packaged_app") {
+        chrome.management.launchApp(extensionId);
+      }
+    });
+  });
 }
 
-chrome.commands.onCommand.addListener(function(command) {
-	if (command === "reload") {
-		reloadExtensions();
-	}
-});
+function reloadExtensions() {
+  // find all zensight extensions and reload them
+  chrome.management.getAll(function(a) {
+    var ext = {};
+    for (var i = 0; i < a.length; i++) {
+      ext = a[i];
+      if ((ext.enabled === true) && /Zensight/.test(ext.name)) {
+          console.log(ext.name + " reloaded");
+          reloadExtension(ext.id, ext.type);
+      }
+    }
+  });
 
-// intercept url
-chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
-		if (details.url.indexOf("http://reload.extensions") >= 0) {
-			reloadExtensions();
-			// Close the tab if we can find a valid associated tab
-			chrome.tabs.get(details.tabId, function(tab) {
-				// This is usually a pre-fetch request for which no tab is associated.
-				if (typeof(tab) === "undefined") {
-					// Check lastError to prevent chrome from whining
-					var lastError = chrome.runtime.lastError;
-					if (lastError && lastError.message.slice(0, 15) != "No tab with id:") {
-						console.log("reload.extensions weirdness:" + chrome.runtime.lastError.message);
-					}
-					return;
-				}
-				chrome.tabs.remove(details.tabId);
-			});
-		}
+  // show an "OK" badge
+  chrome.browserAction.setBadgeText({text: "OK!"});
+  chrome.browserAction.setBadgeBackgroundColor({color: "#3291e7"});
+  setTimeout(function() { chrome.browserAction.setBadgeText({text: ""}); }, 1000);
+}
 
-	return {cancel: false};
-	},
-	{
-		urls: ["http://reload.extensions/"],
-		types: ["main_frame"]
-	},
-	["blocking"]
-);
+chrome.webRequest.onCompleted.addListener(
+  reloadExtensions,
+  {urls: ["https://*.zsinternal.com/*/assume-user*",
+          "https://app.zensight.co/*/assume-user*"],
+  types: ["main_frame", "sub_frame"]});
 
-chrome.browserAction.onClicked.addListener(function(/*tab*/) {
-	reloadExtensions();
-});
+chrome.browserAction.onClicked.addListener(reloadExtensions);
